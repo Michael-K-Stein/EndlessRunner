3ccfe41ce4da341dd8a299c75d741efa1cd2bc0d
Hanich18 <hanich18@aram.cloud>
2021-11-15 17:04:46 +0200

    *minor bug fixes*

--- Testing/Physics/player.py
+++ Testing/Physics/player.py
@@ -5,6 +5,9 @@
 from enum import Enum
 import logging, sys
 
+TRANSITION_MOVE_COUNT = 30
+JUMP_V0_FORCE = 10
+GRAVITY_FORCE = -10
 
 class Player_PositionY_Modes(Enum):
     DEFAULT = 0
@@ -16,6 +19,7 @@ class Player_PositionX_Modes(Enum):
     LEFT = 1
     RIGHT = 2
 
+
 class Player:
 
     player_width = -1
@@ -25,6 +29,10 @@ class Player:
     pos_world_y = -1
     pos_x = Player_PositionX_Modes.DEFAULT
     pos_y = Player_PositionY_Modes.DEFAULT
+    target_location_x = -1
+    target_location_y = -1
+
+    transition_move_counter = -1
 
     screen_block_heights = -1
     screen_block_widths = -1
@@ -40,21 +48,33 @@ class Player:
         self.player_width = self.screen_block_widths
         self.player_height = 2 * self.screen_block_heights
 
+    def update(self):
+        if pos_world_y != target_location_y:
+            if self.pos_y = Player_PositionY_Modes.JUMPED:
+                self.pos_world_y = physics_world_jump_function(transition_move_counter, 0, JUMP_V0_FORCE, GRAVITY_FORCE)
+            else:
+                self.pos_world_y += ((self.pos_world_y - self.target_location_y)/abs(self.pos_world_y - self.target_location_y))
+
+    def physics_world_jump_function(self,time:int, y_0:float, v_0:float, a:float):
+        return y_0 + (v_0*time) + ((a*pow(time,2))/2)
+
     def jump(self):
         if self.pos_y != Player_PositionY_Modes.JUMPED:
-            self.pos_world_y += self.screen_block_heights
+            #self.pos_world_y += self.screen_block_heights
             self.pos_y = Player_PositionY_Modes.JUMPED
+            self.target_location_y = 2*self.screen_block_heights
             logging.debug('INFO : \t\tPLAYER : jump : ')
         else:
             logging.debug('WARNING : \t\tPLAYER : jump : Player tried to jump while midair! WTF.👺')
 
     def tuck(self):
         if self.pos_x != Player_PositionX_Modes.TUCKED:
-            self.pos_world_y -= self.screen_block_heights
+            #self.pos_world_y -= self.screen_block_heights
             self.pos_y = Player_PositionY_Modes.TUCKED
+            self.target_location_y = 0
             logging.debug('INFO : \t\tPLAYER : tuck : ')
         else:
             logging.debug('WARNING : \t\tPLAYER : tuck : Player tried to tuck while already tucked!')
 
     def post_location():
-        self.location_post_callback(self.pos_world_x, self.pos_world_y, self.pos_y, self.pos_x)
+        self.location_post_callback(self.pos_world_x, self.pos_world_y)
