830ac798f80d15c12925cea8413d759d7116a223
hanich007 <hanich07@aram.cloud>
2021-11-15 17:41:19 +0200

    Birds go brrr

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ timer = ClockObject()
 class EndlessRunner(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        # Load the environment model        
+        # Load the environment model
         self.scene = self.loader.loadModel("models/tunnel/tunnel.egg.pz")
         # Reparent the model to render.
         self.scene.reparentTo(self.render)
@@ -19,45 +19,34 @@ class EndlessRunner(ShowBase):
         self.scene.setPos(0, 0, 0)
         self.scene.setHpr(180, 90, 270)
 
+        self.birds = []
+
         # Camera setup
-        #self.camera.setHpr(90, 0, 0)
-        #self.camera.setPos(100, 100, 0)
+        self.taskMgr.add(self.setup_camera, "SetupCamera")
+
+        # Spawn bird
         self.accept("space", self.spawn_bird, ["fire", 1])
 
-        # Add the spinCameraTask procedure to the task manager.
-        #self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
-        #self.taskMgr.
-        self.taskMgr.add(self.setupCamera, "SetupCamera")
+        # Game loop
+        self.taskMgr.add(self.game_loop, "GameLoop")
 
-        
+    def game_loop(self, task):
+        for bird in self.birds:
+            bird.setPos(bird, -0.2, 0, 0)
 
-    def setupCamera(self, task):
-        # if int(task.time) % 3 == 0:
-            # print(task.time)
-        self.camera.setHpr(90, 0, 0)
         return Task.cont
 
-    # Define a procedure to move the camera.
-    def spinCameraTask(self, task):
-        angleDegrees = task.time * 30.0
-        print(angleDegrees)
-        angleRadians = angleDegrees * (pi / 180.0)
-        if int(angleDegrees) % 50 != 0:
-            self.camera.setHpr(90, 0, 0)
-            return Task.cont
-        #self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
-        self.camera.setHpr(angleDegrees, 0, 0)
-        
-        return Task.cont
-
-
-    # spawn a bird
     def spawn_bird(self, key, val):
-        print("Spawn")
         bird = self.loader.loadModel("models/birds/12214_Bird_v1max_l3.obj")
         bird.reparentTo(self.scene)
         bird.setPos(0, 0, -100)
-        bird.setHpr(90, 0, 0)
+        bird.setHpr(90, 0, 90)
+        self.birds.append(bird)
+
+    def setup_camera(self, task):
+        self.camera.setHpr(90, 0, 0)
+        return Task.cont
+
 
 app = EndlessRunner()
 app.run()
