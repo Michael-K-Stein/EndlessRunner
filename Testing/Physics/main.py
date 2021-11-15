4a882f46bf02d20eb28bd350a543a351763f617e
Hanich18 <hanich18@aram.cloud>
2021-11-15 15:18:32 +0200

    Create main.py
    *minor bug fixes*

---
+++ Testing/Physics/main.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+
+
+def player_jump_callback():
+    pass
+
+
+def player_tuck_callback():
+    pass
+
+
+def player_move_right_callback():
+    pass
+
+
+def player_move_left_callback():
+    pass
+
+
+if __name__ == '__main__': 
+    main()
