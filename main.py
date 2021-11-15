9e1bd4428066b5b2e6e0711764324825dc8c2226
Frisco-227 <frisco-227@greendreamteam.onmicrosoft.com>
2021-11-15 18:08:40 +0200

    Ralph go brrrr

--- main.py
+++ main.py
@@ -1,69 +1,152 @@
-from direct.showbase.Loader import Loader
 from direct.showbase.ShowBase import ShowBase
-from direct.task import Task
-from math import pi, sin, cos
+from panda3d.core import Fog
+from panda3d.core import TextNode
+from direct.gui.OnscreenText import OnscreenText
+from direct.showbase.DirectObject import DirectObject
+from direct.interval.MetaInterval import Sequence
+from direct.interval.LerpInterval import LerpFunc
+from direct.interval.FunctionInterval import Func
 from direct.actor.Actor import Actor
+from direct.task import Task
 from panda3d.core import ClockObject
 
+# Global variables for the tunnel dimensions and speed of travel
+TUNNEL_SEGMENT_LENGTH = 50
+TUNNEL_TIME = 2  # Amount of time for one segment to travel the
+# distance of TUNNEL_SEGMENT_LENGTH
 BIRD_SPAWN_INTERVAL_SECONDS = 3
 
 bird_spawner_timer = ClockObject()
 
-class EndlessRunner(ShowBase):
+class DinoRender(ShowBase):
+
+    # Macro-like function used to reduce the amount to code needed to create the
+    # on screen instructions
+    def genLabelText(self, i, text):
+        return OnscreenText(text=text, parent=base.a2dTopLeft, scale=.05,
+                            pos=(0.06, -.065 * i), fg=(1, 1, 1, 1),
+                            align=TextNode.ALeft)
+
     def __init__(self):
+        # Initialize the ShowBase class from which we inherit, which will
+        # create a window and set up everything we need for rendering into it.
         ShowBase.__init__(self)
-        # Load the environment model
-        self.scene = self.loader.loadModel("models/tunnel/tunnel.egg.pz")
-        # Reparent the model to render.
-        self.scene.reparentTo(self.render)
-        # Apply scale and position transforms on the scene.
-        self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(0, 0, 0)
-        self.scene.setHpr(180, 90, 270)
+
+        # Standard initialization stuff
+        # Standard title that's on screen in every tutorial
+        self.title = OnscreenText(text="NSSWS", style=1,
+            fg=(1, 1, 1, 1), shadow=(0, 0, 0, .5), parent=base.a2dBottomRight,
+            align=TextNode.ARight, pos=(-0.1, 0.1), scale=.08)
+
+        # disable mouse control so that we can place the camera
+        base.disableMouse()
+        camera.setPosHpr(0, 0, 10, 0, -90, 0)
+        base.setBackgroundColor(0, 0, 0)  # set the background color to black
 
         self.birds = []
-
-        # Camera setup
-        self.taskMgr.add(self.setup_camera, "SetupCamera")
-
-        # Spawn birds
         self.taskMgr.add(self.bird_spawner, "BirdSpawner")
-
-        # Game loop
         self.taskMgr.add(self.game_loop, "GameLoop")
+
+        # World specific-code
+        self.fog = Fog('distanceFog')
+        self.fog.setColor(0, 0, 0)
+        self.fog.setExpDensity(.045)
+        # We will set fog on render which means that everything in our scene will
+        # be affected by fog. Alternatively, you could only set fog on a specific
+        # object/node and only it and the nodes below it would be affected by
+        # the fog.
+        render.setFog(self.fog)
+
+        # Load the tunel and start the tunnel
+        self.initTunnel()
+        self.initRalph()
+        self.contTunnel()
 
     def game_loop(self, task):
         for bird in self.birds:
-            bird.setPos(bird, -0.2, 0, 0)
+            bird.setPos(bird, -1.5, 0, 0)
        
         return Task.cont
 
-    
+    # Code to initialize the tunnel
+    def initTunnel(self):
+        self.tunnel = [None] * 4
+
+        for x in range(4):
+            # Load a copy of the tunnel
+            self.tunnel[x] = loader.loadModel('models/tunnel')
+            # The front segment needs to be attached to render
+            if x == 0:
+                self.tunnel[x].reparentTo(render)
+            # The rest of the segments parent to the previous one, so that by moving
+            # the front segement, the entire tunnel is moved
+            else:
+                self.tunnel[x].reparentTo(self.tunnel[x - 1])
+            # We have to offset each segment by its length so that they stack onto
+            # each other. Otherwise, they would all occupy the same space.
+            self.tunnel[x].setPos(0, 0, -TUNNEL_SEGMENT_LENGTH)
+            # Now we have a tunnel consisting of 4 repeating segments with a
+            # hierarchy like this:
+            # render<-tunnel[0]<-tunnel[1]<-tunnel[2]<-tunnel[3]
+
+    def initRalph(self):
+        self.ralph = Actor("models/ralph",
+                           {"run": "models/ralph-run",
+                            "walk": "models/ralph-walk"})
+        self.ralph.reparentTo(render)
+        self.ralph.setScale(.15)
+        self.ralph.setPos(0, -1, 5.5)
+        self.ralph.setHpr(0, -90, 0)
+        self.ralph.setH(0)
+        self.ralph.setH(self.ralph, 180)
+        self.ralph.loop('run')
+
+    # This function is called to snap the front of the tunnel to the back
+    # to simulate traveling through it
+    def contTunnel(self):
+        # This line uses slices to take the front of the list and put it on the
+        # back. For more information on slices check the Python manual
+        self.tunnel = self.tunnel[1:] + self.tunnel[0:1]
+        # Set the front segment (which was at TUNNEL_SEGMENT_LENGTH) to 0, which
+        # is where the previous segment started
+        self.tunnel[0].setZ(0)
+        # Reparent the front to render to preserve the hierarchy outlined above
+        self.tunnel[0].reparentTo(render)
+        # Set the scale to be apropriate (since attributes like scale are
+        # inherited, the rest of the segments have a scale of 1)
+        self.tunnel[0].setScale(.155, .155, .305)
+        # Set the new back to the values that the rest of teh segments have
+        self.tunnel[3].reparentTo(self.tunnel[2])
+        self.tunnel[3].setZ(-TUNNEL_SEGMENT_LENGTH)
+        self.tunnel[3].setScale(1)
+
+        print('hello')
+
+        # Set up the tunnel to move one segment and then call contTunnel again
+        # to make the tunnel move infinitely
+        self.tunnelMove = Sequence(
+            LerpFunc(self.tunnel[0].setZ,
+                     duration=TUNNEL_TIME,
+                     fromData=0,
+                     toData=TUNNEL_SEGMENT_LENGTH * .305), # speed
+            Func(self.contTunnel)
+        )
+        self.tunnelMove.start()
+
     def bird_spawner(self, task):
         if (int(bird_spawner_timer.getRealTime()) + 1) % BIRD_SPAWN_INTERVAL_SECONDS == 0:
             self.spawn_bird()
             bird_spawner_timer.reset()
         return Task.cont
-
-
-    def setupCamera(self, task):
-        self.camera.setHpr(90, 0, 0)
-        return Task.cont
-
-    # spawn a bird
+    
     def spawn_bird(self):
         print("Spawn")
         bird = self.loader.loadModel("models/birds/12214_Bird_v1max_l3.obj")
-        bird.reparentTo(self.scene)
-        bird.setPos(0, 0, -100)
+        bird.reparentTo(render)
+        bird.setPos(0, -0.49, -5)
+        bird.setScale(.03)
         bird.setHpr(90, 0, 90)
         self.birds.append(bird)
-
-    def setup_camera(self, task):
-        self.camera.setHpr(90, 0, 0)
-        return Task.cont
-
-
-app = EndlessRunner()
-app.run()
-app.camera.setHpr(90, 0, 0)
+      
+demo = DinoRender()
+demo.run()
